ca577b3fdcd451a4d165045e0e60696d2fb08791
Ruby <bautisruby643@gmail.com>
2020-12-22 01:08:15 -0500

    made buildcard function- not getting any input though

--- lib/screens/dailyProgress.dart
+++ lib/screens/dailyProgress.dart
@@ -11,14 +11,12 @@ class DailyProgress extends StatefulWidget {
 
 class DailyProgressState extends State<DailyProgress> {
   Stream<QuerySnapshot> stream =
-  FirebaseFirestore.instance.collection("physiological").snapshots();
+      FirebaseFirestore.instance.collection("physiological").snapshots();
 
-  int _radioValue1 = -1;
   int correctScore = 0;
 
   @override
   Widget build(BuildContext context) {
-
     return StreamBuilder<QuerySnapshot>(
         stream: stream,
         builder: (context, snapshot) {
@@ -34,92 +32,19 @@ class DailyProgressState extends State<DailyProgress> {
                     "Happy Day",
                     textAlign: TextAlign.center,
                     style:
-                    TextStyle(fontWeight: FontWeight.bold, fontSize: 35.0),
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 35.0),
                   ),
                 ),
                 body: Container(
                     child: ListView(
                         children:
-                        List.generate(snapshot.data.docs.length, (index) {
-                          DocumentSnapshot currentDocument = snapshot.data.docs[index];
-                          return Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: <Widget>[
-                              Card(
-                                color: Color(0xFFF9E79F),
-                                child: Column(children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.all(4.0),
-                                  ),
-                                  Text(currentDocument["qa1"]),
-                                  Padding(
-                                    padding: const EdgeInsets.all(4.0),
-                                  ),
-                                  new Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: <Widget>[
-                                      new Text(
-                                        'Disagree',
-                                        style: new TextStyle(fontSize: 16.0),
-                                      ),
-                                      Transform.scale(
-                                        scale: 2,
-                                        child: new Radio(
-                                          activeColor: Color(0xFFAB1C0F),
-                                          value: 0,
-                                          groupValue: _radioValue1,
-                                          onChanged: _handleRadioValueChange1,
-                                        ),
-                                      ),
-                                      Transform.scale(
-                                        scale: 1.5,
-                                        child: new Radio(
-                                          activeColor: Colors.red,
-                                          value: 1,
-                                          groupValue: _radioValue1,
-                                          onChanged: _handleRadioValueChange1,
-                                        ),
-                                      ),
-                                      Transform.scale(
-                                        scale: 1,
-                                        child: new Radio(
-                                          value: 2,
-                                          groupValue: _radioValue1,
-                                          onChanged: _handleRadioValueChange1,
-                                        ),
-                                      ),
-                                      Transform.scale(
-                                        scale: 1.5,
-                                        child: new Radio(
-                                          activeColor: Colors.green,
-                                          value: 3,
-                                          groupValue: _radioValue1,
-                                          onChanged: _handleRadioValueChange1,
-                                        ),
-                                      ),
-                                      Transform.scale(
-                                        scale: 2,
-                                        child: new Radio(
-                                          activeColor: Color(0xFF125F20),
-                                          value: 4,
-                                          groupValue: _radioValue1,
-                                          onChanged: _handleRadioValueChange1,
-                                        ),
-                                      ),
-                                      new Text(
-                                        'Agree',
-                                        style: new TextStyle(fontSize: 16.0),
-                                      ),
-                                    ],
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(4.0),
-                                  ),
-                                ]),
-                              ),
-                            ],
-                          );
-                        }))),
+                            List.generate(snapshot.data.docs.length, (index) {
+                  DocumentSnapshot currentDocument = snapshot.data.docs[index];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[buildQuestionCard(currentDocument)],
+                  );
+                }))),
               );
             default:
               return Text("Loading data");
@@ -127,7 +52,174 @@ class DailyProgressState extends State<DailyProgress> {
         });
   }
 
-  void _handleRadioValueChange1(int value) {
+  Widget buildQuestionCard(DocumentSnapshot currentDocument) {
+    int _radioValue1 = -1;
+
+    return Card(
+      color: Color(0xFFF9E79F),
+      child: Column(children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+        ),
+        Text(currentDocument["qa1"]),
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+        ),
+        new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'Disagree',
+              style: new TextStyle(fontSize: 16.0),
+            ),
+            Transform.scale(
+              scale: 2,
+              child: new Radio(
+                activeColor: Color(0xFFAB1C0F),
+                value: 0,
+                groupValue: _radioValue1,
+                onChanged: (int value) {
+                  setState(() {
+                    _radioValue1 = value;
+
+                    switch (_radioValue1) {
+                      case 0:
+                        correctScore++;
+                        break;
+                      case 1:
+                        break;
+                      case 2:
+                        break;
+                      case 3:
+                        break;
+                      case 4:
+                        break;
+                    }
+                  });
+                },
+              ),
+            ),
+            Transform.scale(
+              scale: 1.5,
+              child: new Radio(
+                activeColor: Colors.red,
+                value: 1,
+                groupValue: _radioValue1,
+                onChanged: (int value) {
+                  setState(() {
+                    _radioValue1 = value;
+
+                    switch (_radioValue1) {
+                      case 0:
+                        correctScore++;
+                        break;
+                      case 1:
+                        break;
+                      case 2:
+                        break;
+                      case 3:
+                        break;
+                      case 4:
+                        break;
+                    }
+                  });
+                },
+              ),
+            ),
+            Transform.scale(
+              scale: 1,
+              child: new Radio(
+                value: 2,
+                groupValue: _radioValue1,
+                onChanged: (int value) {
+                  setState(() {
+                    _radioValue1 = value;
+
+                    switch (_radioValue1) {
+                      case 0:
+                        correctScore++;
+                        break;
+                      case 1:
+                        break;
+                      case 2:
+                        break;
+                      case 3:
+                        break;
+                      case 4:
+                        break;
+                    }
+                  });
+                },
+              ),
+            ),
+            Transform.scale(
+              scale: 1.5,
+              child: new Radio(
+                activeColor: Colors.green,
+                value: 3,
+                groupValue: _radioValue1,
+                onChanged: (int value) {
+                  setState(() {
+                    _radioValue1 = value;
+
+                    switch (_radioValue1) {
+                      case 0:
+                        correctScore++;
+                        break;
+                      case 1:
+                        break;
+                      case 2:
+                        break;
+                      case 3:
+                        break;
+                      case 4:
+                        break;
+                    }
+                  });
+                },
+              ),
+            ),
+            Transform.scale(
+              scale: 2,
+              child: new Radio(
+                activeColor: Color(0xFF125F20),
+                value: 4,
+                groupValue: _radioValue1,
+                onChanged: (int value) {
+                  setState(() {
+                    _radioValue1 = value;
+
+                    switch (_radioValue1) {
+                      case 0:
+                        correctScore++;
+                        break;
+                      case 1:
+                        break;
+                      case 2:
+                        break;
+                      case 3:
+                        break;
+                      case 4:
+                        break;
+                    }
+                  });
+                },
+              ),
+            ),
+            new Text(
+              'Agree',
+              style: new TextStyle(fontSize: 16.0),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+        ),
+      ]),
+    );
+  }
+
+  /*void _handleRadioValueChange1(int value) {
     setState(() {
       _radioValue1 = value;
 
@@ -145,5 +237,5 @@ class DailyProgressState extends State<DailyProgress> {
           break;
       }
     });
-  }
+  }*/
 }
